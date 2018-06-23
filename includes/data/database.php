2c21d77971666343d4c48628214ec18c802f748c
dart2112 <DNAblue2112@gmail.com>
2018-06-23 21:36:11 +0930

    Working on intergrating with UUIDs and LapisBans

--- includes/data/database.php
+++ includes/data/database.php
@@ -3,6 +3,7 @@ $dbhost = 'localhost';
 $dbuser = 'root';
 $dbpass = 'password';
 $dbname = 'database';
+$offline = false;
 $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
